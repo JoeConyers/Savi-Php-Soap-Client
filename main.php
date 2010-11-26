c708b064e8427d5525d305016fab701b7da9db1d
jbc <jbc@jbcs-Mac.local>
2010-11-26 10:44:50 -0800

    response reporting in

--- main.php
+++ main.php
@@ -5,9 +5,10 @@ $savi_username = "admin";
 $savi_password = "S@v!@pi187";
 
 
-$savi_client = new SoapClient("http://@66.77.198.194/sc-ws/services/ReportService?wsdl",      array('login'  => "admin", 'password' => "S@v!@pi187"));
+$savi_client = new SoapClient("http://66.77.198.194/sc-ws/services/ReportService?wsdl",      array('login'  => "admin", 'password' => "S@v!@pi187"));
  
 
-var_dump($savi_client);
+var_dump($savi_client->__getFunctions());
+
 
 ?>
