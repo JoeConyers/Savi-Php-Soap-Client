db4aca642b6801725da7f79e80016a570a72783d
jbc <jbc@jbcs-Mac.local>
2010-11-26 14:18:30 -0800

    created a call for Savi's timestamp fn

--- main.php
+++ main.php
@@ -1,14 +1,15 @@
 <?php
 
-debug_print_backtrace();
 $savi_username = "admin";
 $savi_password = "S@v!@pi187";
 
+$options=  array('login'  => $savi_username, 'password' => $savi_password);
 
-$savi_client = new SoapClient("http://66.77.198.194/sc-ws/services/ReportService?wsdl",      array('login'  => $savi_username, 'password' => $savi_password));
+$savi_client = new SoapClient("http://66.77.198.194/sc-ws/services/ReportService?wsdl",  $options);
  
 
-var_dump($savi_client->__getFunctions());
+//var_dump($savi_client->__getFunctions());
+var_dump($savi_client->__soapCall("GetSystimestampFromDual", array("param1" => $value1)));
 
 
 ?>
